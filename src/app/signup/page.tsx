48e2ca5cfb0ae945ea4d871cbf6f42314b14ea67
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-06-07 12:36:19 -0300

    ♻️ refatoração do registro e validação do registro

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,5 @@
 import type { Metadata } from "next";
 
-import { Suspense } from "react";
-
-import { Splash } from "@components";
-
 import RegisterScreen from "./screen";
 
 export const metadata: Metadata = {
@@ -12,9 +8,5 @@ export const metadata: Metadata = {
 };
 
 export default function Register() {
-  return (
-    <Suspense fallback={<Splash />}>
-      <RegisterScreen />
-    </Suspense>
-  );
+  return <RegisterScreen />;
 };
--- src/app/signup/screen/index.tsx
+++ src/app/signup/screen/index.tsx
@@ -1,63 +1,101 @@
 "use client"
 
-import { Button, Input } from "@components";
+import { Suspense, useState } from "react";
+
+import { useRouter } from "next/navigation";
 
 import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 
-import { User } from "@assets";
+import { api } from "@services/api";
+import { Button, Input, Splash } from "@components";
 
 import { ColorHeader, Container, Form, InputRow } from "./styles";
 import { schema, schemaProps } from "./schema";
 
+type StatusProps = {
+  loading: boolean;
+  status: 'primary' | 'error';
+};
+
 export default function RegisterScreen() {
-  const { control, formState: { isValid } } = useForm<schemaProps>({
+  const route = useRouter();
+
+  const [status, setStatus] = useState({} as StatusProps);
+
+  const { control, handleSubmit, formState: { isValid } } = useForm<schemaProps>({
     resolver: yupResolver(schema),
-    // defaultValues: {},
+    defaultValues: {
+      name: 'sergio junio leal',
+      email: 'ssergiojunioleal@gmail.com',
+      password: '123456789',
+      confirmPassword: '123456789',
+    },
     // mode: 'onChange'
   });
 
+  async function submit(form: schemaProps) {
+    setStatus({ loading: true, status: 'primary' });
+
+    const data = await api.user.register(form);
+
+    if (Object.values(data)[0] === 404) {
+      return setStatus({ loading: false, status: 'error' });
+    };
+
+    return route.push('/login');
+  };
+
   return (
-    <Container>
+    <Suspense fallback={<Splash />}>
+      <Container>
 
-      <ColorHeader />
+        <ColorHeader />
 
-      <Form>
-        <Controller
-          name='fullname'
-          control={control}
-          render={({ field: { value, onChange } }) => (
-            <Input icon={User} label="Nome Completo" value={value ?? ''} onChange={onChange} />
-          )}
-        />
-        <Controller
-          name='email'
-          control={control}
-          render={({ field: { value, onChange } }) => (
-            <Input icon={User} label="Email" value={value ?? ''} onChange={onChange} />
-          )}
-        />
-        <InputRow>
+        <Form onSubmit={handleSubmit(submit)}>
           <Controller
-            name='password'
+            name='name'
             control={control}
             render={({ field: { value, onChange } }) => (
-              <Input icon={User} type="password" label="Senha" value={value ?? ''} onChange={onChange} />
+              <Input label="Nome Completo" value={value ?? ''} onChange={onChange} />
             )}
           />
           <Controller
-            name='confirmPassword'
+            name='email'
             control={control}
             render={({ field: { value, onChange } }) => (
-              <Input icon={User} type="password" label="Repita a Senha" value={value ?? ''} onChange={onChange} />
+              <Input label="Email" value={value ?? ''} onChange={onChange} />
             )}
           />
-        </InputRow>
-        <div style={{ height: 10 }} />
-        <Button type="submit" disabled={!isValid}>Enviar</Button>
-        <div style={{ height: 10 }} />
+          <InputRow>
+            <Controller
+              name='password'
+              control={control}
+              render={({ field: { value, onChange } }) => (
+                <Input type="password" label="Senha" value={value ?? ''} onChange={onChange} />
+              )}
+            />
+            <Controller
+              name='confirmPassword'
+              control={control}
+              render={({ field: { value, onChange } }) => (
+                <Input type="password" label="Repita a Senha" value={value ?? ''} onChange={onChange} />
+              )}
+            />
+          </InputRow>
+          <div style={{ height: 10 }} />
+          <Button
+            type="submit"
+            $variant={status.status}
+            loading={status.loading}
+            disabled={!isValid || status.loading}
+          >
+            {status.status === 'error' ? 'Tente Novamente' : 'Registrar'}
+          </Button>
+          <div style={{ height: 10 }} />
 
-      </Form>
-    </Container>
+        </Form>
+      </Container>
+    </Suspense>
   )
 };
