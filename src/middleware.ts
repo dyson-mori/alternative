025a01ef460f8dfab4a17e966e42713561ad5af4
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-06-07 12:30:12 -0300

    ✨ implementando jwt

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,12 @@
-export default function middleware() {
+import { NextResponse } from 'next/server';
 
+// const PUBLIC_ROUTES = ['/login', '/signup'];
+// const SECRET = process.env.JWT_SECRET as string;
+
+export function middleware() {
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ['/profile', '/login', '/signup'],
+};
