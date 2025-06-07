900b7064f16b7b4198a90bfbc4e2e10f42d6a03f
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-06-07 12:54:42 -0300

    ✨ novo validador jwt

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,22 @@
+import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
-// const PUBLIC_ROUTES = ['/login', '/signup'];
-// const SECRET = process.env.JWT_SECRET as string;
+import { jwtVerify } from 'jose';
 
-export function middleware() {
+// const PUBLIC_ROUTES = ['/login', '/signup'];
+const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET!); // 1862eda0-dfa4-45e2-8147-e2f15b2d583e
+
+export async function middleware() {
+  const cookie = await cookies();
+  const token = cookie.get('alternative-token');
+
+  if (!token?.value) {
+    return NextResponse.redirect(new URL('/login'));
+  };
+
+  const { payload } = await jwtVerify(token?.value!, JWT_SECRET);
+  console.log({ payload });
+
   return NextResponse.next();
 }
 
