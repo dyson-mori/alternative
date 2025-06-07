247bbfadc1467866cc99743215f56cada79287bb
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-06-07 12:31:44 -0300

    ♻️ reajustando a estrutura

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,14 @@ import { Montserrat } from "next/font/google";
 
 import Template from "../template";
 
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: "--font-montserrat",
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const dynamic = 'force-dynamic';
+
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
@@ -13,9 +21,3 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
     </html>
   );
 };
-
-const montserrat = Montserrat({
-  subsets: ['latin'],
-  variable: "--font-montserrat",
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-});
