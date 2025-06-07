401318821f4e74a4a9a66e73cb43929570b3cd08
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-06-07 12:31:13 -0300

    ✨ implementando tipagens

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,21 +1,30 @@
 import fetcher from "@utils/fetcher";
 
+type RegisterError = {
+  status: number;
+  statusText: string;
+};
+
 type UserLoginProps = {
   name: string;
   email: string;
   password: string;
 };
 
+type UserResponse = boolean | string | RegisterError;
+
 type ApiProps = {
   user: {
-    login: (body: Pick<UserLoginProps, 'email' | 'password'>) => Promise<string>;
-    authentication: (id: string) => Promise<UserLoginProps>;
+    login: (body: Pick<UserLoginProps, 'email' | 'password'>) => Promise<UserResponse>;
+    register: (body: UserLoginProps) => Promise<UserResponse>;
+    authentication: () => Promise<UserLoginProps>;
   };
 };
 
 export const api: ApiProps = {
   user: {
     login: (body) => fetcher({ url: '/user/auth', method: 'POST', body }),
-    authentication: (id) => fetcher({ url: `/user/auth?id=${id}`, method: 'GET' }),
+    register: (body) => fetcher({ url: '/user', method: 'POST', body }),
+    authentication: () => fetcher({ url: `/user/auth`, method: 'GET' }),
   }
 };
