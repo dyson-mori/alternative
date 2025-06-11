ef7b02ea189b783ac529cb38578475404cfb72a8
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-06-11 18:26:47 -0300

    ✨ mudança na exportação

--- src/utils/renderWithTheme.tsx
+++ src/utils/renderWithTheme.tsx
@@ -3,14 +3,15 @@ import { ThemeProvider } from 'styled-components';
 
 import theme from '@global/theme'; // ou onde estiver
 
-const renderWithTheme = (ui: React.ReactElement, options = {}) =>
-  render(<ThemeProvider theme={theme}>{ui}</ThemeProvider>, options);
+const renderWithTheme = (ui: React.ReactElement, options = {}) => {
+  return render(<ThemeProvider theme={theme}>{ui}</ThemeProvider>, options);
+};
 
 export * from '@testing-library/react';
 
-function decodeToken(token: string) {
+export function decodeToken(token: string) {
   const payload = token.split('.')[1];
   return JSON.parse(atob(payload));
 };
 
-export { renderWithTheme, decodeToken };
+export default renderWithTheme;
