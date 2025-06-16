4f451bcd9c88ea642a3d08c3a56703397c76d32f
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-06-16 11:01:35 -0300

    🧪 logo para testes

---
+++ src/assets/logo.tsx
@@ -0,0 +1,22 @@
+export const SvgLogo = (
+  <svg width="89" height="79" viewBox="0 0 89 79" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_d_915_775)">
+      <path d="M6.49502 12.2503C4.76297 9.2503 6.92838 5.4997 10.3925 5.4997L76.21 5.50036C79.6741 5.50036 81.8393 9.24985 80.1075 12.2498L47.1983 69.2502C45.5203 72.1563 41.4042 72.247 39.5739 69.5223L39.4044 69.2496L6.49502 12.2503Z" stroke="#41B06E" strokeWidth="3" />
+    </g>
+    <defs>
+      <filter id="filter0_d_915_775" x="0.383362" y="0" width="87.8358" height="79" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+        <feOffset dx="1" dy="1" />
+        <feGaussianBlur stdDeviation="2.5" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0.254902 0 0 0 0 0.690196 0 0 0 0 0.431373 0 0 0 1 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_915_775" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_915_775" result="shape" />
+      </filter>
+    </defs>
+  </svg>
+);
+
+/**
+ *  criei para evitar com que os testes quebrassem.
+ * */
